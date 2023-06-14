fc862a5562ebc9ab775a0286d4e1fc3646c62ff5
dSvyat <dima3vozniuk@gmail.com>
2023-06-14 13:56:26 +0200

    minor bugs fixed

--- src/main/java/ua/vozniuk/AirlinesProject/services/PersonDetailService.java
+++ src/main/java/ua/vozniuk/AirlinesProject/services/PersonDetailService.java
@@ -1,7 +1,6 @@
 package ua.vozniuk.AirlinesProject.services;
 
-import
-.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
