e8a8edb9d9826ca25aa050a99253c8a4ab76ada0
dSvyat <dima3vozniuk@gmail.com>
2023-06-14 13:53:38 +0200

    minor bugs fixed

--- src/main/java/ua/vozniuk/AirlinesProject/controllers/AuthController.java
+++ src/main/java/ua/vozniuk/AirlinesProject/controllers/AuthController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import ua.vozniuk.AirlinesProject.util.PersonValidator;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final PersonValidator personValidator;
+    private final Validator personValidator;
     private final RegistrationService registrationService;
 
     @Autowired
